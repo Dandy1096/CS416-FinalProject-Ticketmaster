2dd595a45edf6ef0acbf44cb3bf2bce29e930397
Dandy1096 <sausagesauce2@gmail.com>
2025-12-11 11:13:30 -0500

    Fixed regression that borked STATIC_ROOT

--- TicketProject/settings.py
+++ TicketProject/settings.py
@@ -116,11 +116,11 @@ APPS_DIR = BASE_DIR/"Application"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = BASE_DIR/'static'
+# STATIC_ROOT = BASE_DIR/'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
